4393a8c20c7a54970e2c13fa834573780d08f0b3
Sharms911 <132896436+Sharms911@users.noreply.github.com>
2024-03-14 11:42:44 +1030

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ plt.savefig('test_chart')
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/results', methods=["GET", "POST"])
